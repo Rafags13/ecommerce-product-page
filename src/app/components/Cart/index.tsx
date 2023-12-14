15200dab4c4bf2e63ae32480cb8d2a2a4610069d
Rafael Veiga <rafael_todescato@hotmail.com>
2023-12-14 12:02:13 -0400

    <fix>: solving problems with style overriding

--- src/app/components/Cart/index.tsx
+++ src/app/components/Cart/index.tsx
@@ -2,19 +2,20 @@ import { useState } from 'react';
 import cart from '../../../../images/icon-cart.svg';
 import styles from './cart.module.css';
 import Content from './Content';
+import Button from '../Button';
 
 
 export default function Cart() {
   const [openedCart, setOpenedCart] = useState(false);
   return (
     <section className={styles.cartContainer}>
-      <button className={styles.button} onClick={() => { setOpenedCart(!openedCart) }}>
+      <Button className={styles.button} onClick={() => { setOpenedCart(!openedCart) }}>
         <div>
           <div className={styles.cartItemsCounting}>3</div>
           <img src={cart} height={20} width={20} />
         </div>
 
-      </button>
+      </Button>
       {openedCart ? (
         <Content />
       ) : null}
